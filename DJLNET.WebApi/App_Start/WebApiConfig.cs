cac811dbaf96ee9026bc0bd60c6ace3e554767bb
杜江龙 <djlnet@outlook.com>
2016-12-24 00:18:00 +0800

    ...

--- DJLNET.WebApi/App_Start/WebApiConfig.cs
+++ DJLNET.WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace DJLNET.WebApi
@@ -10,14 +11,26 @@ namespace DJLNET.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API 配置和服务
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping(queryStringParameterName: "formate", queryStringParameterValue: "json", mediaType: "application/json"));
 
+            config.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping(queryStringParameterName: "formate", queryStringParameterValue: "xml", mediaType: "application/xml"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.JsonFormatter.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "GetAllTests",
+                    id = RouteParameter.Optional
+                }
             );
         }
     }
---
+++ DJLNET.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DJLNET.ApplicationService.Interfaces;
+using DJLNET.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DJLNET.WebApi.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class HomeController : ApiController
+    {
+        private readonly ITestService _testService;
+
+        public HomeController(ITestService testService)
+        {
+            this._testService = testService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Test> GetAllTests()
+        {
+            return _testService.GetAll();
+        }
+
+        [HttpPost]
+        public bool AddTest(string name)
+        {
+            return this._testService.Add(new Test { Name = name });
+        }
+    }
+}
