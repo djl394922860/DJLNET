98432251db11ad687d0b0bd52d01a6ea9de71948
杜江龙 <djlnet@outlook.com>
2016-12-23 22:36:23 +0800

    .

---
+++ DJLNET.Core/Logger/NullLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJLNET.Core.Logger
+{
+    public class NullLogger : ILogger
+    {
+        public void Error(string message)
+        {
+
+        }
+
+        public void Error(string message, Exception innerException)
+        {
+
+        }
+
+        public void Fatal(string message)
+        {
+
+        }
+
+        public void Fatal(string message, Exception innerException)
+        {
+
+        }
+
+        public void Info(string message)
+        {
+
+        }
+
+        public void Info(string message, Exception innerException)
+        {
+
+        }
+
+        public void Warning(string message)
+        {
+
+        }
+
+        public void Warning(string message, Exception innerException)
+        {
+
+        }
+    }
+}
