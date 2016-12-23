89963b34fbeec21de5c6e900935e999645952735
杜江龙 <djlnet@outlook.com>
2016-12-23 23:06:40 +0800

    ............

---
+++ DJLNET.Core/ServiceContainer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJLNET.Core
+{
+    public static class ServiceContainer
+    {
+        private readonly static IUnityContainer Container;
+
+        static ServiceContainer()
+        {
+            Container = new UnityContainer();
+        }
+
+        public static IUnityContainer CurrentContainer
+        {
+            get
+            {
+                return Container;
+            }
+        }
+
+        public static T Resole<T>() where T : class
+        {
+            return CurrentContainer.Resolve<T>();
+        }
+
+        public static T Resole<T>(string registerName) where T : class
+        {
+            return CurrentContainer.Resolve<T>(registerName);
+        }
+
+        public static IEnumerable<T> ResoleAll<T>() where T : class
+        {
+            return CurrentContainer.ResolveAll<T>();
+        }
+
+        public static bool IsRegisted<T>() where T : class
+        {
+            return CurrentContainer.IsRegistered<T>();
+        }
+
+    }
+}
