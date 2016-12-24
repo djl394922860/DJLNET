acdd5c347195e9fbb8b46b8cc4088cf058c1f922
杜江龙 <djlnet@outlook.com>
2016-12-24 16:02:50 +0800

    aaa

--- DJLNET.WebMvc/App_Start/BundleConfig.cs
+++ DJLNET.WebMvc/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace DJLNET.WebMvc
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // 是否启用cdn加速
+            bundles.UseCdn = false;
+            // 是有启用压缩
+            BundleTable.EnableOptimizations = false;
 
         }
     }
