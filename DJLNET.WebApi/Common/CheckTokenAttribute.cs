a8b13e7cf5602e58c23a9787f0205ff0a2bcafe4
杜江龙 <djlnet@outlook.com>
2017-01-23 01:29:51 +0800

    自定义 签名验证过滤器+身份授权验证器

---
+++ DJLNET.WebApi/Common/CheckTokenAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace DJLNET.WebApi.Common
+{
+    public class CheckTokenAttribute : System.Web.Http.Filters.ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+    }
+}
