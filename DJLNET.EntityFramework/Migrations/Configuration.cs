f5ab183d34d2d9655ab993081d292d56641b39e7
杜江龙 <jianglong.du@chukou1.com>
2017-04-29 22:04:40 +0800

    启用数据库迁移

--- DJLNET.EntityFramework/Migrations/Configuration.cs
+++ DJLNET.EntityFramework/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace DJLNET.EntityFramework.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DJLNETDBContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DJLNET.EntityFramework.DJLNETDBContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(DJLNETDBContext context)
+        protected override void Seed(DJLNET.EntityFramework.DJLNETDBContext context)
         {
             //  This method will be called after migrating to the latest version.
 
