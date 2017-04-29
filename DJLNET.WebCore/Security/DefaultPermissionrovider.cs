fbfb573880c27134992341dbd96883159bec4e1f
杜江龙 <jianglong.du@chukou1.com>
2017-04-29 23:47:05 +0800

    添加权限测试ps1脚本

--- DJLNET.WebCore/Security/DefaultPermissionrovider.cs
+++ DJLNET.WebCore/Security/DefaultPermissionrovider.cs
@@ -15,6 +15,9 @@ namespace DJLNET.WebCore.Security
 
             permissions.Add(new Permission { Name = "UserCreate", Category = "用户管理", Description = "创建", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
 
+            permissions.Add(new Permission { Name = "UserDelete", Category = "用户管理", Description = "删除", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+
             return permissions;
         }
     }
