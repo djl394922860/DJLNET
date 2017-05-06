4d3725838039354326b6a8ee89601e7296c99822
杜江龙 <jianglong.du@chukou1.com>
2017-05-06 18:25:54 +0800

    添加 测试权限。modal dialog 正确姿势

--- DJLNET.WebCore/Security/DefaultPermissionrovider.cs
+++ DJLNET.WebCore/Security/DefaultPermissionrovider.cs
@@ -13,9 +13,31 @@ namespace DJLNET.WebCore.Security
         {
             var permissions = new List<Permission>();
 
-            permissions.Add(new Permission { Name = "UserCreate", Category = "用户管理", Description = "创建", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+            #region User
+            permissions.Add(new Permission { Name = "UserIndex", Category = "用户管理", Description = "查询", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "UserAdd", Category = "用户管理", Description = "增加", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "UserEdit", Category = "用户管理", Description = "修改", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
 
             permissions.Add(new Permission { Name = "UserDelete", Category = "用户管理", Description = "删除", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "UserAuth", Category = "用户管理", Description = "授权", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+            #endregion
+
+
+            #region  Role
+            permissions.Add(new Permission { Name = "RoleIndex", Category = "角色管理", Description = "查询", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "RoleAdd", Category = "角色管理", Description = "增加", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "RoleEdit", Category = "角色管理", Description = "修改", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "RoleDelete", Category = "角色管理", Description = "删除", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            permissions.Add(new Permission { Name = "RoleAuth", Category = "角色管理", Description = "授权", CreatedTime = DateTime.Now, CreatedBy = "system", UpdatedTime = DateTime.Now, UpdatedBy = "system", IsDeleted = false });
+
+            #endregion
 
 
             return permissions;
