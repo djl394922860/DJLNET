c7658eed59cd50d8771036a26c11881440b71f02
杜江龙 <jianglong.du@chukou1.com>
2017-05-06 14:54:38 +0800

    精简代码

--- DJLNET.WebMvc/Controllers/RoleController.cs
+++ DJLNET.WebMvc/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using DJLNET.Core.Paging;
 using DJLNET.Model.Entities;
 using DJLNET.Repository.Interfaces;
 using DJLNET.WebCore.Mvc;
+using DJLNET.WebMvc.Extensions;
 using DJLNET.WebMvc.Models;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using DJLNET.Core.Extension;
 
 namespace DJLNET.WebMvc.Controllers
 {
@@ -32,24 +34,8 @@ namespace DJLNET.WebMvc.Controllers
             Expression<Func<Role, bool>> condition = null;
             if (!string.IsNullOrWhiteSpace(query.Name))
                 condition = x => x.Name.Contains(query.Name);
-            int pageNum = (query.Start / query.Length) + 1;
-            IPagedList<Role> data = _service.PagingQuery(condition, pageNum, query.Length, query.OrderBy, query.OrderDir == DataTablesOrderDir.Desc);
-
-            var count = data.Total;
-            var result = data.Rows;
-
-            return DataTablesJsonResult(query.Draw, count, count, result);
-        }
-
-        public JsonResult DataTablesJsonResult<TEntity>(int draw, int recordsTotal, int recordsFiltered,
-           IReadOnlyList<TEntity> data, string error = null) where TEntity : class, new()
-        {
-            var result = new DataTablesResult<TEntity>(draw, recordsFiltered, recordsFiltered, data);
-            var jsonResult = new JsonNetResult
-            {
-                Data = result
-            };
-            return jsonResult;
+            IPagedList<Role> data = _service.PagingQuery(condition, (query.Start / query.Length) + 1, query.Length, query.OrderBy, query.OrderDir == DataTablesOrderDir.Desc);
+            return DataTablesExtensions.DataTablesJsonResult(query.Draw, data.Total, data.Total, data.Rows);
         }
     }
 }
---
+++ DJLNET.WebMvc/Extensions/DataTablesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DJLNET.WebCore.Mvc;
+using DJLNET.WebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DJLNET.WebMvc.Extensions
+{
+    public static class DataTablesExtensions
+    {
+        public static JsonResult DataTablesJsonResult<TEntity>(int draw, int recordsTotal, int recordsFiltered,
+          IReadOnlyList<TEntity> data, string error = null) where TEntity : class, new()
+        {
+            var result = new DataTablesResult<TEntity>(draw, recordsFiltered, recordsFiltered, data);
+            var jsonResult = new JsonNetResult
+            {
+                Data = result
+            };
+            return jsonResult;
+        }
+    }
+}
