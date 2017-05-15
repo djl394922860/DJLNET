da92c486a724ebc1751b588331a996236ae4e44f
杜江龙 <jianglong.du@chukou1.com>
2017-05-15 21:48:01 +0800

    增加菜单导航实体类，更新数据库

---
+++ DJLNET.EntityFramework/Mappings/NavigateMapping.cs
@@ -0,0 +1,24 @@
+﻿using DJLNET.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJLNET.EntityFramework.Mappings
+{
+    public class NavigateMapping : System.Data.Entity.ModelConfiguration.EntityTypeConfiguration<Navigate>
+    {
+        public NavigateMapping()
+        {
+            this.HasKey(x => x.ID);
+            this.Property(x => x.ActionName).HasMaxLength(32).IsRequired();
+            this.Property(x => x.ControllerName).HasMaxLength(32).IsRequired();
+            this.Property(x => x.Name).HasMaxLength(32).IsRequired();
+            this.Property(x => x.IconClassCode).HasMaxLength(32).IsRequired();
+            this.Property(x => x.Active).IsRequired();
+            this.Property(x => x.SortOrder).IsOptional();
+            this.HasOptional(x => x.Parent).WithMany(x => x.Children).HasForeignKey(x => x.ParentID);
+        }
+    }
+}
---
+++ DJLNET.Model/Entities/Navigate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJLNET.Model.Entities
+{
+    public class Navigate : GenericEntity<int>
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string Name { get; set; }
+        public string IconClassCode { get; set; }
+        public int? ParentID { get; set; }
+        public virtual Navigate Parent { get; set; }
+        public bool Active { get; set; }
+        public int? SortOrder { get; set; }
+        public virtual ICollection<Navigate> Children { get; set; } = new List<Navigate>();
+    }
+}
